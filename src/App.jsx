4eb4ecf0eae200967f39402c0494f823e7b47ee9
Mokekula <dmitriy.sedykh33@gmail.com>
2021-09-02 23:23:07 +0300

    fix: some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,61 @@
-import { useState } from "react";
-import Header from "./components/Header/Header";
-import Map from "./components/Map/Map";
-import Profile from "./components/Profile/Profile";
-import Registration from "./components/Registration/Registration";
-import Login from "./components/Login/Login";
-import Back from "./img/registration-background.png";
-import AuthContext from "./contex/AuthContext";
+import { useState } from 'react';
+import Header from './components/Header/Header';
+import Map from './components/Map/Map';
+import Profile from './components/Profile/Profile';
+import Registration from './components/Registration/Registration';
+import Login from './components/Login/Login';
+import Back from './img/registration-background.png';
+import AuthContext from './contex/AuthContext';
 
 function App() {
-  const [router, setRouter] = useState("registration");
-
+  const [router, setRouter] = useState('registration');
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  
   const logout = () => {
     setIsLoggedIn(false);
-    setRouter("registration");
+    setRouter('registration');
   };
-
+  
   const login = (event) => {
     let authData = {
       email: event.target.email.value,
       password: event.target.password.value,
     };
-
+    
     setIsLoggedIn(true);
-    setRouter("map");
+    setRouter('map');
   };
-
+  
   const handlePage = (event) => {
     setRouter(event.target.value);
   };
-
+  
   const backImg = () => {
     return Back;
   };
-
+  
   return (
     <div className="App">
       <AuthContext.Provider
-        value={{
+        value={ {
           logout,
           login,
-        }}
+        } }
       >
-        {router === "login" ? null : router === "registration" ? null : (
-          <Header handlePage={handlePage} />
-        )}
-
-        {router === "map" && isLoggedIn === true ? (
-          <Map />
-        ) : router === "registration" ? (
-          <Registration back={backImg} handleLogin={handlePage} />
-        ) : router === "profile" && isLoggedIn === true ? (
-          <Profile />
+        { router === 'login' ? null : router === 'registration' ? null : (
+          <Header handlePage={ handlePage }/>
+        ) }
+        
+        { router === 'map' && isLoggedIn === true ? (
+          <Map/>
+        ) : router === 'registration' ? (
+          <Registration back={ backImg } handlePage={ handlePage }/>
+        ) : router === 'profile' && isLoggedIn === true ? (
+          <Profile/>
         ) : (
-          <Login handleRegister={handlePage} />
-        )}
+          <Login handlePage={ handlePage }/>
+        ) }
       </AuthContext.Provider>
     </div>
   );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,22 @@
-import React, { useContext } from "react";
-import "./Login.styles.scss";
-import Back from "../../img/registration-background.png";
-import PropTypes from "prop-types";
-import AuthContext from "../../contex/AuthContext";
+import React, { useContext } from 'react';
+import './Login.styles.scss';
+import Back from '../../img/registration-background.png';
+import PropTypes from 'prop-types';
+import AuthContext from '../../contex/AuthContext';
 
 const Login = ({ handlePage }) => {
   const { login } = useContext(AuthContext);
+  
   return (
     <div
       className="login"
-      style={{
-        backgroundImage: "url(" + Back + ")",
-      }}
+      style={ {
+        backgroundImage: 'url(' + Back + ')',
+      } }
     >
       <div className="container">
         <div className="login-logo">
-          <img src="loft__taxi-img.png" alt="" width="156px" />
+          <img src="loft__taxi-img.png" alt="" width="156px"/>
         </div>
         <div className="login-form">
           <div className="login-title">Войти</div>
@@ -23,18 +24,18 @@ const Login = ({ handlePage }) => {
             <span> Новый пользователь? </span>
             <button
               className="login__signup-link"
-              value="login"
-              onClick={handlePage}
+              value="registration"
+              onClick={ handlePage }
             >
               Зарегистрируйтесь
             </button>
           </div>
           <div className="login__signin">
-            <form onSubmit={(event) => login(event)}>
+            <form onSubmit={ (event) => login(event) }>
               <div className="login__signin-email">
                 <label htmlFor="">
                   Имя пользователя *
-                  <br />
+                  <br/>
                   <input
                     type="text"
                     className="email-input"
@@ -43,16 +44,16 @@ const Login = ({ handlePage }) => {
                   />
                 </label>
               </div>
-
+              
               <div className="login__signin-password">
                 <label htmlFor="">
                   Пароль *
-                  <br />
-                  <input type="password" name="password" required />
+                  <br/>
+                  <input type="password" name="password" required/>
                 </label>
               </div>
               <div className="login__signin-submit">
-                <input type="submit" value="Зарегистрироваться" required />
+                <input type="submit" value="Зарегистрироваться" required/>
               </div>
             </form>
           </div>
